3950bcfbaf2c6614ef311d4686c982726a93a404
Lars Willemsens <lars.willemsens@kdg.be>
2016-08-13 11:10:08 +0200

    Added a (very) simple model.

--- smvc_thymeleaf_account/src/main/java/be/kdg/prog3/thymeleafaccount/controller/AccountController.java
+++ smvc_thymeleaf_account/src/main/java/be/kdg/prog3/thymeleafaccount/controller/AccountController.java
@@ -3,14 +3,18 @@ package be.kdg.prog3.thymeleafaccount.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import be.kdg.prog3.thymeleafaccount.model.Account;
 
 @Controller
 public class AccountController {
     @GetMapping("/show")
     public ModelAndView showAccountBalance() {
-        ModelAndView modelAndView = new ModelAndView();
+        final Account account = new Account("Marcel");
+        account.setBalance(37.5);
+
+        final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("showAccountBalance");
-        modelAndView.getModel().put("account", 7);
+        modelAndView.getModel().put("account", account);
         return modelAndView;
     }
 }
