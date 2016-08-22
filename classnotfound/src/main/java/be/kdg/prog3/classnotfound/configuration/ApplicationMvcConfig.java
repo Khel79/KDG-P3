5a51dc5e02fc5f92e66480d10a49d236eaf722ef
Lars Willemsens <lars.willemsens@kdg.be>
2016-08-22 13:33:34 +0200

    Added spring security dialect for thymeleaf.

--- classnotfound/src/main/java/be/kdg/prog3/classnotfound/configuration/ApplicationMvcConfig.java
+++ classnotfound/src/main/java/be/kdg/prog3/classnotfound/configuration/ApplicationMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
@@ -39,6 +40,7 @@ public class ApplicationMvcConfig extends WebMvcConfigurerAdapter implements App
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setEnableSpringELCompiler(true);
         engine.setTemplateResolver(templateResolver());
+        engine.addDialect(new SpringSecurityDialect());
         return engine;
     }
 
