75023264af58683b89a8998a1226dee8fdd4413f
Lars Willemsens <lars.willemsens@kdg.be>
2016-08-22 10:52:57 +0200

    Added basic URL redirection after lgin.

--- classnotfound/src/main/java/be/kdg/prog3/classnotfound/configuration/ApplicationSecurityConfig.java
+++ classnotfound/src/main/java/be/kdg/prog3/classnotfound/configuration/ApplicationSecurityConfig.java
@@ -1,14 +1,17 @@
 package be.kdg.prog3.classnotfound.configuration;
 
 import be.kdg.prog3.classnotfound.model.UserRepository;
+import be.kdg.prog3.classnotfound.security.LoginSuccessHandler;
 import be.kdg.prog3.classnotfound.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -30,9 +33,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .loginPage("/login")
+                .successHandler(successHandler())
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/q").authenticated()
+                .antMatchers(HttpMethod.POST, "/q")
+                .authenticated()
                 .anyRequest().permitAll();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new LoginSuccessHandler("/");
     }
 }
