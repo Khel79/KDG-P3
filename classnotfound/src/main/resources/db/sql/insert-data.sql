d98b711fb3db54e0e20b781c6eba33179458cce7
Lars Willemsens <lars.willemsens@kdg.be>
2016-09-06 12:00:48 +0200

    Added some extra test data.

--- classnotfound/src/main/resources/db/sql/insert-data.sql
+++ classnotfound/src/main/resources/db/sql/insert-data.sql
@@ -1,7 +1,41 @@
 INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
-  VALUES ('Hoe laat is het?', 'Dat heb ik me altijd al afgevraagd.', 20, '2016-09-30 14:26:00');
+  VALUES ('Hoe laat is het?', 'Graag zo snel mogelijk antwoord!', 20, '2016-09-30 14:47:00');
 INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
-  VALUES ('Welke dag zijn we vandaag?', 'Graag zo snel mogelijk antwoord!', 16, '2016-09-30 13:01:00');
+  VALUES ('Hoe werkt Spring?', 'Graag zo snel mogelijk antwoord!', 16, '2016-09-30 11:34:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Wat is Gradle?', 'Graag zo snel mogelijk antwoord!', 45, '2016-09-30 10:29:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Waarom gebruiken we geen Maven?', 'Graag zo snel mogelijk antwoord!', 10, '2016-09-30 18:18:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Wat is een repository?', 'Graag zo snel mogelijk antwoord!', 9, '2016-09-30 05:41:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Hoe werkt de @Component tag?', 'Graag zo snel mogelijk antwoord!', 78, '2016-09-30 14:22:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Hoe werkt de @Bean tag?', 'Graag zo snel mogelijk antwoord!', 34, '2016-09-30 16:00:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Wat is een controller?', 'Graag zo snel mogelijk antwoord!', 65, '2016-09-30 09:43:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Hoe kan ik in Thymeleaf X Y Z doen?', 'Graag zo snel mogelijk antwoord!', 12, '2016-09-30 00:34:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Wat is de standaard directory structuur van een Maven/Gradle project?', 'Graag zo snel mogelijk antwoord!', 4, '2016-09-30 21:15:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Help, mijn SpEL expressie werkt niet...', 'Graag zo snel mogelijk antwoord!', 55, '2016-09-30 15:12:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Waar kan ik de Tomcat log files vinden?', 'Graag zo snel mogelijk antwoord!', 24, '2016-09-30 16:35:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Hoe configureer ik Spring MVC?', 'Graag zo snel mogelijk antwoord!', 14, '2016-09-30 13:45:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Hoe zet ik mijn project om naar Spring Boot?', 'Graag zo snel mogelijk antwoord!', 62, '2016-09-30 17:08:00');
+INSERT INTO question (que_subject, que_body, que_score, que_timestamp)
+  VALUES ('Mijn .war file werkt niet.... Help! :(', 'Graag zo snel mogelijk antwoord!', 30, '2016-09-30 10:45:00');
 
 INSERT INTO user (use_name, use_email, use_password)
   VALUES ('lars', 'lars@somehost.com', 'lars');
+INSERT INTO user (use_name, use_email, use_password)
+  VALUES ('rudy', 'rudy@somehost.com', 'rudy');
+INSERT INTO user (use_name, use_email, use_password)
+  VALUES ('marcel', 'marcel@somehost.com', 'marcel');
+INSERT INTO user (use_name, use_email, use_password)
+  VALUES ('jos', 'jos@somehost.com', 'jos');
+INSERT INTO user (use_name, use_email, use_password)
+  VALUES ('pedro', 'pedro@somehost.com', 'pedro');
