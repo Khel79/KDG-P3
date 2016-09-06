ab5ebc335bfd46587a7ca264b5d032dd854f39ac
Lars Willemsens <lars.willemsens@kdg.be>
2016-09-06 10:56:31 +0200

    Simplified the links with anchor tags, simplified CSS and removed custom JS.

--- classnotfound/src/main/resources/client/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("td.subject").click(
-        function () {
-            var id = $(this).parent().attr('id');
-            console.log("click!" + id);
-        }
-    );
-});
