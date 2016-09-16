7b77d354e5e7255e381c38ca6ba1a4ad7ec658fe
Lars Willemsens <lars.willemsens@kdg.be>
2016-09-16 15:17:03 +0200

    Added a basic filter example.

---
+++ filters/src/main/java/be/kdg/prog3/filters/MyFirstFilter.java
@@ -0,0 +1,29 @@
+package be.kdg.prog3.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/"}, filterName = "first")
+public class MyFirstFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request.getAttribute("defaultName") == null) {
+            request.setAttribute("defaultName", "Jos");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ filters/src/main/java/be/kdg/prog3/filters/MySecondFilter.java
@@ -0,0 +1,28 @@
+package be.kdg.prog3.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/"}, filterName = "second")
+public class MySecondFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request.getAttribute("defaultName") != null) {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
