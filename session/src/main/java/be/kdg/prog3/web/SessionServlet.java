0bbfc2c3f3ab02b4cc371f0f1e7185227d82a4af
Lars Willemsens <lars.willemsens@kdg.be>
2016-09-24 20:54:56 +0200

    Added a very basic session example.

---
+++ session/src/main/java/be/kdg/prog3/web/SessionServlet.java
@@ -0,0 +1,33 @@
+package be.kdg.prog3.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/submit")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String data = req.getParameter("data");
+        if (data != null) {
+            // Put the data in the session.
+            final HttpSession session = req.getSession();
+            session.setAttribute("data", data);
+            final int time = Integer.parseInt(req.getParameter("time"));
+            session.setMaxInactiveInterval(time);
+        }
+
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String data = (String)req.getSession().getAttribute("data");
+        resp.setContentType("text/plain");
+        resp.getWriter().print("Je data is \"" + (data == null ? "" : data) + "\".");
+    }
+}
