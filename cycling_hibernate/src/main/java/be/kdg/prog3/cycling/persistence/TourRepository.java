969e937db0fb1c38309da7e0ca993118bba44415
Lars Willemsens <lars.willemsens@kdg.be>
2017-09-30 15:45:43 +0200

    Removed unused classes.

--- cycling_hibernate/src/main/java/be/kdg/prog3/cycling/persistence/TourRepository.java
+++
@@ -1,7 +0,0 @@
-package be.kdg.prog3.cycling.persistence;
-
-import be.kdg.prog3.cycling.model.Tour;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TourRepository extends CrudRepository<Tour, Long> {
-}
